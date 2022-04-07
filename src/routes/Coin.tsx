9985a2268e3ecc48e1117b3f271b2a80aa4dd9d7
aiden <zero2one23581@gmail.com>
2022-04-07 17:29:50 +0900

    ⚡️ Data Types

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -5,15 +5,71 @@ import styled from "styled-components";
 interface ILocation {
   state: string;
 }
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    ath_date: string;
+    ath_price: number;
+    market_cap: number;
+    market_cap_change_24h: number;
+    percent_change_1h: number;
+    percent_change_1y: number;
+    percent_change_6h: number;
+    percent_change_7d: number;
+    percent_change_12h: number;
+    percent_change_15m: number;
+    percent_change_24h: number;
+    percent_change_30d: number;
+    percent_change_30m: number;
+    percent_from_price_ath: number;
+    price: number;
+    volume_24h: number;
+    volume_24h_change_24h: number;
+  };
+}
+
 function Coins() {
   const { coinId } = useParams();
   const [loading, setLoading] = useState(false);
   const { state } = useLocation() as ILocation;
 
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
-  console.log(info);
   console.log(priceInfo);
 
   useEffect(() => {
@@ -34,7 +90,7 @@ function Coins() {
       <header>
         <h1>{state}</h1>
       </header>
-      <main>{loading ? <span>{}</span> : <h1>"Loading..."</h1>}</main>
+      <main>{loading ? null : <h1>"Loading..."</h1>}</main>
     </Container>
   );
 }
