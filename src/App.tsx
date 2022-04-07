5c7eb0c3ed76f409317c465141b503df933a9017
aiden <zero2one23581@gmail.com>
2022-04-07 13:26:43 +0900

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import styled from "styled-components";
+import Router from "./router";
 
 function App() {
-  return (
-    <Container>
-      <H1>HELLO WORLD</H1>
-      <button>START</button>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  button {
-    padding: 10px 20px;
-    border: none;
-    border-radius: 10px;
-    font-size: 2rem;
-    background-color: inherit;
-    color: ${(props) => props.theme.textColor};
-    &:hover {
-      background-color: ${(props) => props.theme.btnColor};
-      color: black;
-    }
-  }
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-  font-size: 4rem;
-`;
