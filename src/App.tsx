c20371bffa367af824c616c04d21664524e3776d
aiden <zero2one23581@gmail.com>
2022-04-12 16:52:08 +0900

    🔥 Set up for New PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import Router from "./router";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { darkTheme, lightTheme } from "./theme";
-import { useRecoilValue } from "recoil";
-import { isDarkAtom } from "./atoms";
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </ThemeProvider>
+      <GlobalStyle />
     </>
   );
 }
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,7 +2,6 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
-    //여기 안에서 테마정의를 확장함.
     textColor: string;
     bgColor: string;
     accentColor: string;
