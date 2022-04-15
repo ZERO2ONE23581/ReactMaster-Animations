9e25eed2cdb19e98dfa9d9a950e7f6eb8a726a53
aiden <zero2one23581@gmail.com>
2022-04-15 21:10:07 +0900

    gestures 2

--- framer-animation/src/App.tsx
+++ framer-animation/src/App.tsx
@@ -1,40 +1,53 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const boxVars = {
   hover: {
-    scale: 2,
     rotateZ: 90,
   },
   click: {
-    scale: 1,
     borderRadius: "100%",
-  },
-  drag: {
-    backgroundColor: "rgb(46, 204, 113)",
-    transition: {
-      duration: 5,
-    },
   },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVars} whileHover="hover" whileTap="click" drag whileDrag="drag" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          dragSnapToOrigin
+          dragElastic={0.5}
+          variants={boxVars}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
 export default App;
 
-const Box = styled(motion.div)`
+const BiggerBox = styled.div`
+  background-color: rgb(236, 240, 241);
+  width: 600px;
+  height: 600px;
   border-radius: 40px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  align-items: center;
+  border-radius: 40px;
   width: 200px;
   height: 200px;
-  background-color: white;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 const Wrapper = styled.div`
   height: 100vh;
