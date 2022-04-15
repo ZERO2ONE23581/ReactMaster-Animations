566eaf3e80640e99a86340f824dca4ee3bafbbb4
aiden <zero2one23581@gmail.com>
2022-04-15 23:42:18 +0900

    Slider part one

--- framer-animation/src/App.tsx
+++ framer-animation/src/App.tsx
@@ -3,47 +3,71 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 
 const boxVars = {
-  initial: {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 0.5,
+    },
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
-    y: 100,
+    scale: 0,
+    transition: {
+      duration: 0.5,
+    },
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const previousPlease = () =>
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   //
   return (
     <Wrapper>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVars}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={boxVars}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
-      <button onClick={toggleShowing}>Click</button>
+      <div>
+        <button onClick={nextPlease}>&rarr;</button>
+        <button onClick={previousPlease}>&larr;</button>
+      </div>
     </Wrapper>
   );
 }
 export default App;
 
 const Box = styled(motion.div)`
+  position: absolute;
+  top: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   margin: 30px 0;
-  width: 600px;
-  height: 600px;
+  width: 200px;
+  height: 200px;
   border-radius: 20px;
   box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
   background-color: rgba(255, 255, 255, 0.4);
@@ -57,12 +81,23 @@ const Wrapper = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  button {
-    background-color: rgba(255, 255, 255, 0.1);
-    width: 50%;
-    padding: 20px;
-    font-size: 1.2rem;
-    border-radius: 10px;
-    border: none;
+  div {
+    &:nth-of-type(2) {
+      position: absolute;
+      width: 200px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+      button {
+        width: 100%;
+        padding: 10px 20px;
+        font-size: 28px;
+        border: none;
+        border-radius: 10px;
+        box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+        background-color: rgba(255, 255, 255, 0.4);
+      }
+    }
   }
 `;
