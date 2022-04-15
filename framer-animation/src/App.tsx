de90e43809ddc557274bcf32c9508d75cd46dad0
aiden <zero2one23581@gmail.com>
2022-04-16 00:38:42 +0900

    Layout Prop

--- framer-animation/src/App.tsx
+++ framer-animation/src/App.tsx
@@ -2,76 +2,46 @@ import styled from 'styled-components';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 
-const boxVars = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-};
-
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const previousPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => {
+    setClicked((prev) => !prev);
   };
   //
   return (
-    <Wrapper>
-      <AnimatePresence exitBeforeEnter custom={back}>
-        <Box
-          custom={back}
-          variants={boxVars}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <div>
-        <button onClick={nextPlease}>&rarr;</button>
-        <button onClick={previousPlease}>&larr;</button>
-      </div>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: '20px' }} />
+        ) : null}
+      </Box>
+      <Box>
+        {!clicked ? (
+          <Circle
+            layoutId="circle"
+            style={{ borderRadius: '50%', scale: '2' }}
+          />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
 export default App;
 
+const Circle = styled(motion.div)`
+  background-color: #00a1ff;
+  width: 100px;
+  height: 100px;
+  box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+`;
+
 const Box = styled(motion.div)`
-  position: absolute;
-  top: 200px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
-  margin: 30px 0;
-  width: 200px;
-  height: 200px;
-  border-radius: 20px;
+  width: 400px;
+  height: 400px;
+  border-radius: 50px;
   box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
   background-color: rgba(255, 255, 255, 0.4);
 `;
@@ -81,26 +51,6 @@ const Wrapper = styled.section`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  div {
-    &:nth-of-type(2) {
-      width: 200px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 10px;
-      button {
-        width: 100%;
-        padding: 10px 20px;
-        font-size: 28px;
-        border: none;
-        border-radius: 10px;
-        box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
-        background-color: rgba(255, 255, 255, 0.4);
-      }
-    }
-  }
 `;
